cfeae2e5b73369ad6d4ad03086f96596f2bcd663
ZhenyeLee <73271404+ZhenyeLee@users.noreply.github.com>
2021-07-13 13:20:54 -0500

    Update loss.py
    Add image gradient loss function

--- Pytorch/loss.py
+++ Pytorch/loss.py
@@ -3,3 +3,19 @@ import torch
 def l1_criterion(y_pred,y_true):
   #compute point-wise L1 loss
   l_depth = torch.mean(torch.abs(y_pred - y_true))
+  
+#Lgrad(y, ŷ)
+import torch.nn.functional as func
+def image_gradients(image):
+  #compute image gradient loss
+  left = image
+  right = func.pad(image,[0, 1, 0, 0])[:, :, :, 1:]
+  top = image
+  bottom = func.pad(image, [0, 0, 0, 1])[:, :, 1:, :]
+
+  dx = right - left
+  dy = bottom - top
+  dx[:, :, :, -1] = 0
+  dy[:, :, -1, :] = 0
+
+  return dx,dy
